31a6ee5fe8635c2f4613780618c72ec463ad530a
eod940 <eod940@gmail.com>
2022-09-15 10:41:41 +0900

    (feature/accounts) 기능추가: User 모델링, admin에 추가

--- applications/accounts/admin.py
+++ applications/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from applications.accounts.models import User
+
+
+@admin.register(User)
+class AccountsAdmin(admin.ModelAdmin):
+    pass
